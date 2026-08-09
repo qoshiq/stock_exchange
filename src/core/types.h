5dbc2c8f74de83c4047d00d479b0ba381e51c6b8
Kaushik <helloqoshiq@gmail.com>
2026-08-10 00:08:12 +0530

    initial commit: stock_exchange

---
+++ src/core/types.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "algorithm"
+#include "array"
+#include "cstdint"
+#include "string_view"
+#include "type_traits"
+
+namespace exchange::core {
+
+    using Price  = std::int64_t;
+
+}
